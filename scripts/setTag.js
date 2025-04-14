89e4399f3e2c1efa0795305dd9d162f0d90b2305
Rodrigo-Montenegro-Farias <59695256+Rodrigomontenegrofarias@users.noreply.github.com>
2025-04-14 14:23:55 -0400

    Update setTag.js

--- scripts/setTag.js
+++ scripts/setTag.js
@@ -5,10 +5,5 @@ const fs = require('fs');
 // Obtener el último tag
 const latestTag = execSync('git describe --tags `git rev-list --tags --max-count=1`').toString().trim();
 
-// Crear contenido JS para exportar la versión
-const content = `const version = '${latestTag}';\nexport default version;\n`;
-
-// Escribir en src/version.js
-fs.writeFileSync('src/version.js', content);
-
-console.log('✔️ src/version.js creado con el tag:', latestTag);
+// Escribirlo en un archivo .env.local
+fs.writeFileSync('src/latestTag.txt', latestTag);
